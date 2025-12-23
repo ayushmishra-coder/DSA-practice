ea458acbd8322680574ca316463fe477b20e8330
AyushMishra <ayush945kumar@gmail.com>
2025-12-23 16:28:42 +0530

    learnt first program:I/O in cpp

---
+++ learn_the_basics/things_to_know_in_any_language(c++)/user_input_output/output_with_cout.cpp
@@ -0,0 +1,7 @@
+#include<iostream>  //for Input/Output
+//#include<math.h>  //for math's fn
+int main(){
+    //to print output we use 'cout' from iostream lib.,we write std::cout since it belongs to standard(std)-namespace
+    std::cout << "Hey Ayush!";
+    return 0;
+}
