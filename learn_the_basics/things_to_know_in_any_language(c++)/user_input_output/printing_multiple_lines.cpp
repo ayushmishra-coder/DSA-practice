1b0fe45037889ad73ac33eea3ffbc0f8d8d94f0f
AyushMishra <ayush945kumar@gmail.com>
2025-12-23 17:23:46 +0530

    printing two words suing \n

---
+++ learn_the_basics/things_to_know_in_any_language(c++)/user_input_output/printing_multiple_lines.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main(){
+    // If you write cout statements repeatedly without newline(\n), they print on the same line.ELSE:output of following happens
+    std::cout << "Hey Ayush!\n";
+    std::cout << "Welcome to DSA.";
+    return 0;
+}
